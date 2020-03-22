c121f3e3b57364f3f02425403675602c4b8bc3b6
2754github <fkesys@gmail.com>
2020-03-22 17:46:28 +0900

    Change method

--- 4.PermCheck.py
+++ 4.PermCheck.py
@@ -1,3 +1,10 @@
 def solution(A):
     n = len(A)
-    return int(0.5*n*(n+1) == sum(A))
+    B = list(range(1, n + 1))
+    l = len(B)
+    for a in A:
+        if a - 1 < l:
+            B[a - 1] = 0
+        else:
+            break
+    return int(B.count(0) == l)
